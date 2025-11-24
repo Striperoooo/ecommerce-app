0a42d0f27c5827e66ba50cbcd22bfbf988880e1c
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-11-24 20:08:22 +0800

    Add FeaturedProductYX1 component to promotions
    Introduces the FeaturedProductYX1 component and integrates it into the PromotionalProducts section, displaying the YX1 earphones with image, title, and button.

--- src/components/Home/PromotionalProducts.tsx
+++ src/components/Home/PromotionalProducts.tsx
@@ -1,11 +1,13 @@
 import FeaturedProductZX9 from "./PromotionalProducts/FeaturedProductZX9"
 import FeaturedProductZX7 from "./PromotionalProducts/FeaturedProductZX7"
+import FeaturedProductYX1 from "./PromotionalProducts/FeaturedProductYX1"
 
 export default function PromotionalProducts() {
     return (
         <section className="mt-30 mb-10 flex flex-col gap-6">
             <FeaturedProductZX9 />
             <FeaturedProductZX7 />
+            <FeaturedProductYX1 />
         </section>
 
     )
---
+++ src/components/Home/PromotionalProducts/FeaturedProductYX1.tsx
@@ -0,0 +1,30 @@
+import Typography from "../../ui/Typography"
+import Button from "../../ui/Button"
+import imageEarphonesYX1 from "../../../assets/home/mobile/image-earphones-yx1.jpg"
+
+
+export default function FeaturedProductYX1() {
+    return (
+        <div className="flex flex-col gap-6">
+
+            <div
+                style={{ backgroundImage: `url(${imageEarphonesYX1})` }}
+                className="w-full h-[200px] bg-cover bg-center rounded-lg px-6 py-25"
+            >
+            </div>
+
+            <div className="w-full h-[200px] rounded-lg bg-f1f1 px-6 py-10">
+
+                <Typography variant="h4Other" className="text-black">
+                    YX1 EARPHONES
+                </Typography>
+
+                <Button variant="secondary" className="mt-7">
+                    See Product
+                </Button>
+
+            </div>
+
+        </div>
+    )
+}
