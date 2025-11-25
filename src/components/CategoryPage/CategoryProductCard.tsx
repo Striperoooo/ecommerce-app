55c4530c7d79f69e6b5ee6be8a0528be303b73e2
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-11-26 04:15:27 +0800

    Refactor Category page and product card UI
    Updated CategoryPage and CategoryProductCard components to use data from data.json and display product details dynamically. Improved UI structure with Typography and Button components, and replaced placeholder text with actual product information.

--- src/components/CategoryPage/CategoryProductCard.tsx
+++ src/components/CategoryPage/CategoryProductCard.tsx
@@ -1,11 +1,39 @@
-
-
-
+import data from "../../../data.json"
+import Typography from "../ui/Typography"
+import Button from "../ui/Button"
 
 export default function CategoryProductCard() {
+
+    const product4 = data.find(item => item.id === 4)
+
     return (
         <section>
-            <h1 className="text-green-500">I AM THE PRODUCT CARD HELLO!!!</h1>
+
+            <img src={product4?.categoryImage?.mobile} alt={product4?.name} className="" />
+
+            <div className="mt-8 flex flex-col gap-6 text-center items-center">
+
+                {product4?.new &&
+                    <Typography variant="overline" className="text-d8">
+                        NEW PRODUCT
+                    </Typography>
+                }
+
+
+                <Typography variant="h4OtherTight" className="text-black">
+                    {product4?.name}
+                </Typography>
+
+                <Typography variant="p" className="text-black/50">
+                    {product4?.description}
+                </Typography>
+
+                <Button variant="primary" to={`/product/${product4?.slug}`}>
+                    See Product
+                </Button>
+
+            </div>
+
         </section>
     )
 }
--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,14 +1,33 @@
 import { useParams } from "react-router-dom";
 import CategoryProductCard from "../components/CategoryPage/CategoryProductCard";
+import data from "../../data.json"
+import Typography from "../components/ui/Typography";
+
+interface CategoryPageProps {
+    id: number;
+    slug: string;
+    name: string;
+    categoryImage: { mobile: string; tablet: string; desktop: string; };
+    new: boolean;
+    description: string;
+
+}
 
 export default function CategoryPage() {
     const { categoryName } = useParams()
 
     return (
-        <div className="text-bold text-pink-500">
-            I AM THE {categoryName?.toUpperCase()} CATEGORY PAGE!!!
-            <CategoryProductCard />
-        </div>
+        <>
+            <section className="bg-black text-center py-8" >
+                <Typography variant="h4Other" className="text-white">
+                    {categoryName}
+                </Typography>
+            </section>
+
+            <section className="mt-16 px-10 flex flex-col items-center justify-center">
+                <CategoryProductCard />
+            </section>
+        </>
     )
 
 }
