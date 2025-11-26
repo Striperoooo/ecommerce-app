cfa772c1491c03a902254740007682245ad5206b
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-11-27 03:17:10 +0800

    Add CategoryButtons and AboutUsSection to CategoryPage
    Integrated CategoryButtons and AboutUsSection components into CategoryPage for improved navigation and additional information. Updated layout to use a container and adjusted section structure for better organization.

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import CategoryProductCard from "../components/CategoryPage/CategoryProductCard";
 import data from "../../data.json"
 import Typography from "../components/ui/Typography";
+import CategoryButtons from "../components/Home/CategoryButtons";
+import AboutUsSection from "../components/Home/AboutUsSection";
 
 interface Product {
     id: number;
@@ -33,20 +35,37 @@ export default function CategoryPage() {
 
     return (
         <>
+
             <section className="bg-black text-center py-8" >
                 <Typography variant="h4Other" className="text-white">
                     {categoryName}
                 </Typography>
             </section>
 
-            <section className="mt-16 px-6 flex flex-col gap-30 items-center justify-center">
-                {sortedProducts.map((product) => (
-                    <CategoryProductCard
-                        key={product.id}
-                        product={product}
-                    />
-                ))}
-            </section>
+            <div className="container mx-auto px-6">
+
+                <section className="mt-16 flex flex-col gap-30 items-center justify-center">
+
+                    {sortedProducts.map((product) => (
+                        <CategoryProductCard
+                            key={product.id}
+                            product={product}
+                        />
+                    ))}
+
+
+
+                </section>
+
+                <div className="mt-43">
+                    <CategoryButtons />
+                </div>
+
+                <div>
+                    <AboutUsSection />
+                </div>
+
+            </div>
         </>
     )
 
