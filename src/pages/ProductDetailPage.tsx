0a42734902b0c8d36e2550d2f1e78d1dc093997b
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-11-29 23:09:42 +0800

    Implement product detail page with dynamic data
    Replaces placeholder content with a dynamic product detail view. Fetches product data based on the URL slug, displays a 404 message if not found, and renders product details using the DetailCard component.

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,14 +1,40 @@
 import { useParams } from "react-router-dom"
-
-
+import type { Product } from "../types";
+import data from "../data/data.json"
+import DetailCard from "../components/ProductDetailPage/DetailCard";
 
 export default function ProductDetailPage() {
-    const { productName } = useParams()
+    const { productSlug } = useParams<{ productSlug: string }>()
+
+    const currentProduct: Product | undefined = data.find(
+        (product: Product) => product.slug === productSlug
+    )
+
+    if (!currentProduct) {
+        return (
+            <div className="my-10 ml-4">
+                404 - Product Not Found
+            </div>
+        )
+    }
+
+    const {
+        name,
+        image,
+        new: isNew,
+        price,
+        description,
+    } = currentProduct
 
     return (
-        <section>
-            <h1>i am the product detail page for</h1>
-            <h2>{productName}</h2>
+        <section className="px-6">
+            <DetailCard
+                name={name}
+                image={image}
+                isNew={isNew}
+                price={price}
+                description={description}
+            />
         </section>
     )
 }
