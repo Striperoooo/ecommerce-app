61191f1ea9b7966ac2117bdb5c2b7fa0f14b1827
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-12-07 01:05:38 +0800

    Integrate checkout form submission and order confirmation
    Added form submission handling to CheckoutPage, clearing the cart and navigating to the order confirmation page upon successful checkout. The 'Continue & Pay' button now submits the form only when the cart has items.

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,17 +1,46 @@
 import CheckoutForm from "../components/CheckoutPage/CheckoutForm"
 import SummarySection from "../components/CheckoutPage/SummarySection"
 import Typography from "../components/ui/Typography"
-
+import Button from "../components/ui/Button"
+import { useNavigate } from "react-router-dom"
+import { useCart } from "../hooks/useCartHook"
+import type { FormData } from "../components/CheckoutPage/CheckoutForm"
 
 
 export default function CheckoutPage() {
+    const navigate = useNavigate()
+    const { cart, clearCart } = useCart()
+
+    const handleFormSubmit = (formData: FormData) => {
+        console.log("Final Order Data:", {
+            customerInfo: formData,
+            cartItems: cart,
+        });
+
+        clearCart();
+        // Navigate to the "Thank You" order confirmation page
+        navigate('/order-confirmation');
+    }
+
+
     return (
         <>
             <div className="bg-fafa p-6">
-                <CheckoutForm />
+                <CheckoutForm onSubmit={handleFormSubmit} />
 
-                <div className="mt-8">
+                <div className="mt-8 bg-white px-6 py-8 rounded-lg ">
                     <SummarySection />
+
+                    {cart.length > 0 && (
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            form="checkout-form"
+                            className="mt-8 w-full"
+                        >
+                            Continue & Pay
+                        </Button>
+                    )}
                 </div>
             </div>
         </>
