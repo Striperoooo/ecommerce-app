a5c5be163d5baa8a58e78dab7a694ef7b6b22254
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-12-07 22:51:59 +0800

    Add responsive image and layout to AboutUsSection
    Replaced static image with a <picture> element to support tablet and mobile images. Updated layout and typography classes for improved responsiveness and alignment on larger screens.

--- src/components/Home/AboutUsSection.tsx
+++ src/components/Home/AboutUsSection.tsx
@@ -1,4 +1,5 @@
-import imageBestGear from "../../assets/shared/mobile/image-best-gear.jpg"
+import imageBestGearMobile from "../../assets/shared/mobile/image-best-gear.jpg"
+import imageBestGearTablet from "../../assets/shared/tablet/image-best-gear.jpg"
 import Typography from "../ui/Typography"
 
 
@@ -6,17 +7,38 @@ import Typography from "../ui/Typography"
 export default function AboutUsSection() {
     return (
         <section className="my-30">
-            <div>
-                <img src={imageBestGear} alt="Black and white photo of a person modelling a pair of headphones." className="rounded-lg mx-auto" />
 
-                <div className="text-center">
-                    <Typography variant="h4OtherTight" className="text-black mt-10 mb-8">
+            <div>
+
+                <picture>
+                    <source
+                        media="(min-width: 768px)"
+                        srcSet={imageBestGearTablet}
+                    />
+
+                    <img src={imageBestGearMobile}
+                        alt="Black and white photo of a person modelling a pair of headphones." className="rounded-lg mx-auto"
+                    />
+                </picture>
+
+                <div
+                    className="text-center
+                 md:flex md:flex-col md:items-center"
+                >
+                    <Typography
+                        variant="h4OtherTight" className="text-black mt-10 mb-8
+                    md:mt-16 md:text-[2.5rem] md:leading-[2.75rem] md:tracking-[0.089em] md:max-w-[573px]"
+                    >
                         Bringing you the
                         <span className="text-d8"> best </span>
                         audio gear
                     </Typography>
 
-                    <Typography variant="p" className="text-black/50">
+                    <Typography
+                        variant="p"
+                        className="text-black/50
+                    md:max-w-[573px]"
+                    >
                         Located at the heart of Angeles City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
                     </Typography>
                 </div>
