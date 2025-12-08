41e2e469907997f2942ab8c66ca7d395c6359b4e
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-12-08 11:54:02 +0800

    Improve CategoryProductCard responsive design
    Replaced the img tag with a picture element to support tablet images. Updated class names for better spacing and typography on medium screens, enhancing the card's appearance across devices.

--- src/components/CategoryPage/CategoryProductCard.tsx
+++ src/components/CategoryPage/CategoryProductCard.tsx
@@ -19,9 +19,21 @@ export default function CategoryProductCard({ slug,
     return (
         <section>
 
-            <img src={categoryImage.mobile} alt={name} className="rounded-lg mx-auto" />
+            <picture>
+                <source
+                    media="(min-width: 768px)"
+                    srcSet={categoryImage.tablet}
+                />
 
-            <div className="mt-8 flex flex-col gap-6 text-center items-center">
+                <img
+                    src={categoryImage.mobile}
+                    alt={name}
+                    className="rounded-lg mx-auto"
+                />
+            </picture>
+
+            <div className="mt-8 flex flex-col gap-6 text-center items-center
+            md:mt-13">
 
                 {isNew &&
                     <Typography variant="overline" className="text-d8">
@@ -29,12 +41,19 @@ export default function CategoryProductCard({ slug,
                     </Typography>
                 }
 
-
-                <Typography variant="h4OtherTight" className="text-black">
+                <Typography
+                    variant="h4OtherTight"
+                    className="text-black
+                md:text-[2.5rem] md:leading-[2.75rem] md:tracking-[0.089em] md:max-w-[573px]"
+                >
                     {name}
                 </Typography>
 
-                <Typography variant="p" className="text-black/50">
+                <Typography
+                    variant="p"
+                    className="text-black/50
+                md:mt-2 md:max-w-[573px]"
+                >
                     {description}
                 </Typography>
 
